dc7ec348d264cdc43a4bcbfb0afb579459b8f0da
DIRECT0R-ctrl <DominantVortex@proton.me>
2025-12-11 22:14:27 +0100

    testing

---
+++ exercises/php/01PHP_coursera/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Laksimi Aymane PHP</title>
+</head>
+
+<body>
+  <h1>Laksimi Aymane</h1>
+
+  <pre>
+    AAAAA
+    A   A
+    AAAAA
+    A   A 
+    A   A
+</pre>
+
+  <p>
+    <?php
+    echo hash('sha256', 'Laksimi Aymane');
+    ?>
+  </p>
+
+</body>
+
+</html>
