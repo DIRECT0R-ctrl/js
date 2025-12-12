962a453243435e2c0ab4cc4b90a1b67462ec94d3
DIRECT0R-ctrl <DominantVortex@proton.me>
2025-12-12 15:07:33 +0100

    building up and array

---
+++ tests/php/01/index.php
@@ -0,0 +1,22 @@
+<?php
+/*$stuff = array("name" => "aymane",
+	"course" => "wa4e");*/
+	
+	// echo('<pre>'); print_r($stuff); echo('</pre>')
+	
+	/*$thing = FALSE;
+	
+	echo("One\n");
+	echo($thing);
+	echo("Tow\n");
+	print_r($thing);
+	echo("Three\n");
+	var_dump($thing); */
+
+
+	// Building up an array 
+	$ha = array();
+	$ha[] = "aymane";
+	$ha[] = "wa4e";
+	print_r($ha);
+?>
