a8c3ab03badb85328158706897b4e09166c0c7e3
DIRECT0R-ctrl <DominantVortex@proton.me>
2026-01-05 11:35:02 +0100

    understnading how session work (session_get.php)

---
+++ exercises/php/Session/session_get.php
@@ -0,0 +1,21 @@
+<?php
+
+class Session{
+	public function get($key, $default = null)
+	{
+		return $_SESSION[$key] ?? $default;
+	}
+}
+
+
+$_SESSION['user_id'] = 42;
+
+$session = new Session();
+$session->get('user_id', 0);
+
+echo $_SESSION['user_id'] ?? 0;
+echo "\n"
+
+
+
+?>
